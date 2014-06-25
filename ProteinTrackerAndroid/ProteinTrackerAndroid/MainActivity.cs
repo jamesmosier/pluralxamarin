1398f89b4dd7b497453d14e60096bc6f90d0bbd8
James Mosier <jamesdmosier@gmail.com>
2014-06-25 09:29:33 -0400

    added serviceStack references to android project

---
+++ ProteinTrackerAndroid/ProteinTrackerAndroid/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ProteinTrackerAndroid
+{
+	[Activity (Label = "Protein Tracker", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+		}
+	}
+}
+
+
